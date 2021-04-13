dc046b4fe248b6d42aadb6d400cec9fca4aaba6c
Lenab <lenb9660@gmail.com>
2021-04-13 12:12:39 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,166 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.util.*;
+
+//Створіть наступну модель: є політична сила, яка може приймати до своїх лав громадян країни. Політична сила може проводити
+// з’їзди своїх членів та обирати свої керівні органи. Повинна бути можливість подивитися, за кого голосував кожний учасник.
+// або хто голосував за конкретну людину в керуючому органі.  Кожен активіст може пошукати детальну інформацію по різним критеріям
+// серед всього складу однопартійців, крім того є можливість декларувати помісячно свої доходи та дивитися доходи однопартійців за
+// любий період. Повинна бути можливість формувати списки учасників з’їздів, це роблять регіональні партійні структури. Таким чином
+// регіональний орган обирає з своїх лав учасників з’їзді. Повинна бути можливість формування загальних списків учасників з’їздів з можливістю
+// сортування за регіонами, доходами, та по алфавіту.
+
+class Participant{
+ String name;
+ String region;
+ Map<LocalDate, Integer> incomeReg= new HashMap<>();
+    Map<String, String> vote = new HashMap<>();
+ Participant(String name, String region){
+     this.name=name;
+     this.region=region;
+
+ }
+
+
+public void addIncome(LocalDate date, int income){
+    incomeReg.put(date, income);
+}
+
+
+    public void addVote(String who, String forWhom) {
+        vote.put(who, forWhom);
+    }
+
+
+
+
+public void show(){
+     System.out.println("name:" + this.name + " region:" + this.region + "income:" + this.incomeReg);
+}
+
+
+}
+
+
+class RegionalDep {
+    String name;
+
+    List<Participant> participantList = new ArrayList<>();
+
+
+    RegionalDep(String name) {
+        this.name = name;
+    }
+
+    public List<Participant> getParticipantList() {
+        return participantList;
+    }
+
+    public boolean addParticipant(Participant participant) {
+        return participantList.add(participant);
+    }
+
+
+    void searchStudents(List<Participant> participantList, int criteriaNum) {
+        Scanner sc = new Scanner(System.in);
+        boolean done = true;
+        switch (criteriaNum) {
+            case 1: {
+                System.out.println("Input name of participant: ");
+                String searchName = sc.nextLine();
+                for (Participant participant : participantList) {
+                    if (participant.name.equals(searchName)) {
+                        participant.show();
+                        break;
+                    } else done = false;
+                }
+            }
+            break;
+
+            case 2: {
+                System.out.println("Input region of participant: ");
+                int searchRegion = sc.nextInt();
+                for (Participant participant : participantList) {
+                    if (participant.region.equals(searchRegion)) {
+                        participant.show();
+                        break;
+                    } else done = false;
+                }
+            }
+            break;
+        }
+    }
+
+
+    public void show(List<Participant> participantList) {
+        for (Participant participant : participantList) {
+            System.out.println("name: " + participant.name + "region: " + participant.region + "income:" + participant.incomeReg);
+        }
+
+
+    }
+
+
+    class Political {
+        String name;
+        List<RegionalDep> listDepart = new ArrayList<>();
+       List<Congress > congressList = new ArrayList<>();
+
+        Political(String name) {
+            this.name = name;
+        }
+
+
+        public boolean addRegionDepar(RegionalDep reg) {
+            return listDepart.add(reg);
+
+        }
+
+boolean addToCongress(String name, LocalDate date, int kvota){
+            return congressList.add(new Congress(name, date, kvota));
+}
+
+
+
+
+
+    }
+
+    class Congress{
+
+        String name;
+        LocalDate date;
+        int kvota;
+
+        Congress(String name, LocalDate date, int kvota){
+            this.name=name;
+            this.date=date;
+            this.kvota =kvota;
+
+        }
+
+        List<Participant> partList= new ArrayList<>();
+         List<Participant> managerDep=new ArrayList<>();
+
+        boolean addToCongress(Participant part){
+            return partList.add(part);
+
+        }
+        boolean addToManagerDep (Participant part){
+            return  managerDep.add(part);
+        }
+
+
+
+    }
+
+}
+
+
+    public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
